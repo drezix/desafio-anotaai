91928375271328b8db4db034d44b5cdcdc1d51e6
drezix <andre.t.agostinis@gmail.com>
2024-12-14 12:35:03 -0300

    fix: adicionando anotação no arquivo main

--- src/main/java/com/drezix/desafio/DesafioApplication.java
+++ src/main/java/com/drezix/desafio/DesafioApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class DesafioApplication {
 
 	public static void main(String[] args) {
