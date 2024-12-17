71668a6babbc398fc7ba5cca29d9f57004121b80
drezix <andre.t.agostinis@gmail.com>
2024-12-17 15:16:32 -0300

    feat: adicionando constructors, getter e setters da classe Product

--- src/main/java/com/drezix/desafio/domains/products/Product.java
+++ src/main/java/com/drezix/desafio/domains/products/Product.java
@@ -1,16 +1,10 @@
 package com.drezix.desafio.domains.products;
 
 import com.drezix.desafio.domains.categories.Category;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "product")
-@Getter
-@Setter
-@NoArgsConstructor
 public class Product {
     @Id
     private String id;
@@ -19,4 +13,61 @@ public class Product {
     private String ownerId;
     private Integer price;
     private Category category;
+
+    public Product(){}
+
+    public Product(ProductDTO data) {
+        this.title = data.title();
+        this.description = data.description();
+        this.ownerId = data.ownerId();
+        this.price = data.price();
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getOwnerId() {
+        return this.ownerId;
+    }
+
+    public Integer getPrice() {
+        return this.price;
+    }
+
+    public Category getCategory() {
+        return this.category;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
