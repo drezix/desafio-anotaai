c5b17d89f30eb617564b4be85c860970f0c6e00a
drezix <andre.t.agostinis@gmail.com>
2024-12-17 15:10:48 -0300

    fix: adicionando construtor sem parâmetro para o Spring

--- src/main/java/com/drezix/desafio/domains/categories/Category.java
+++ src/main/java/com/drezix/desafio/domains/categories/Category.java
@@ -1,24 +1,18 @@
 package com.drezix.desafio.domains.categories;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "categories")
-@Getter
-@Setter
-@NoArgsConstructor
 public class Category {
     @Id
     private String id;
-
     private String title;
-
     private String description;
-
     private String ownerId;
+
+    public Category() {
+    }
 
     public Category(CategoryDTO categoryDTO) {
         this.title = categoryDTO.title();
